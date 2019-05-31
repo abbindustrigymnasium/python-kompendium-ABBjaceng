e278218e70b291757a8e766f87ef500d00bc84a4
ABBjaceng <43879993+ABBjaceng@users.noreply.github.com>
2019-05-31 21:08:27 +0200

    comment

--- Pytonprodjekt/Frågesport.py
+++ Pytonprodjekt/Frågesport.py
@@ -2,12 +2,12 @@ from random import randint
 import requests
 import random
 
-antalF = 1
-cleanUp = 1
-points = 0
-l = randint(0,49)
+sammaF = []     #en array som ser till att inte samma fråga ställs flera gånger
 
-url = "https://opentdb.com/api.php?amount=50&difficulty=medium&encode=url3986"
+antalF = 1      #hur många frågor koden har stält
+points = 0      #hur många poäng spelaren har
+
+url = "https://opentdb.com/api.php?amount=50&difficulty=medium"     #genom att ta bort allt efter medium så behövde jag inte använda lika många replace
 r = requests.get(url)
 R = r.json()
 
@@ -26,79 +26,72 @@ elif Redo == "Nej":
 else:
     print("Jag tolkar det som ett ja så då drar vi igång!\n")
 
-while antalF <11:
+while antalF <11:       #det går att ändra hur många frågor man vill ha genom att ändra detta värde
+    
     l = randint(0,49)
 
-    Fråga = R["results"][l]["question"]
-    Rätt = R["results"][l]["correct_answer"]
-    Alt = R["results"][l]["incorrect_answers"]
+    if l != sammaF:     #här är funktionen som gör så att inte samma fråga ställs flera gånger
+        sammaF.append(l)
 
-    Fråga = Fråga.replace('%3F','')
-    Fråga = Fråga.replace('%2F','')
-    Fråga = Fråga.replace('%3A','')
-    Fråga = Fråga.replace('%E2','')
-    Fråga = Fråga.replace('%88','')
-    Fråga = Fråga.replace('%92','')
-    Fråga = Fråga.replace('%60','')
-    Fråga = Fråga.replace('%20',' ')
-    Fråga = Fråga.replace('%21','')    
-    Fråga = Fråga.replace('%22','')
-    Fråga = Fråga.replace('%23','')
-    Fråga = Fråga.replace('%2C','')
-    Fråga = Fråga.replace('%24','')
-    Fråga = Fråga.replace('%25','')
-    Fråga = Fråga.replace('%26','')
-    Fråga = Fråga.replace('%27','')
-    Fråga = Fråga.replace('%28','')
-    Fråga = Fråga.replace('%29','')
-    Rätt = Rätt.replace('%20',' ')
-    Rätt = Rätt.replace('%2C','')
-    for items in range(len(Alt)):
-        if "%20" in Alt[items]:
-            Alt[items]= Alt[items].replace("%20"," ")
-    for items in range(len(Alt)):
-        if "%21" in Alt[items]:
-            Alt[items]= Alt[items].replace("%21","")    
-    for items in range(len(Alt)):
-        if "%27" in Alt[items]:
-            Alt[items]= Alt[items].replace("%27","")
-    for items in range(len(Alt)):
-        if "%28" in Alt[items]:
-            Alt[items]= Alt[items].replace("%28","")
-    for items in range(len(Alt)):
-        if "%29" in Alt[items]:
-            Alt[items]= Alt[items].replace("%29","")
+        Fråga = R["results"][l]["question"]
+        Rätt = R["results"][l]["correct_answer"]
+        Alt = R["results"][l]["incorrect_answers"]
 
-    
-    Alt.append(Rätt)
-    random.shuffle(Alt)
-
-    if len(Alt) == 2:
-        print(Fråga+"?" + "\n",Alt[0]," - ",Alt[1])
-
-    elif len(Alt) == 3:
-        print(Fråga+"?" + "\n",Alt[0]," - ",Alt[1]," - ",Alt[2])
-    
-    elif len(Alt) == 4:
-        print(Fråga+"?" + "\n",Alt[0]," - ",Alt[1]," - ",Alt[2]," - ",Alt[3])
-
-    svartf = (input("> "))
-    svartf=svartf.title()
-
-    if svartf == Rätt and antalF<10:
-        antalF +=1
-        points +=1
-        print("Helt rätt, vidare till nästa fråga nummer",antalF,"\n")       
+        Fråga = Fråga.replace('&quot;','')
+        Fråga = Fråga.replace('&ldquo;','')
+        Fråga = Fråga.replace('&rdquo;','')
+        Fråga = Fråga.replace('&#039','')
+                                                    #det krävs fortfarande några replase för att få koden att se bra ut
+        for items in range(len(Alt)):
+            if "&quot;" in Alt[items]:
+                Alt[items]= Alt[items].replace("&quot;","")
+        for items in range(len(Alt)):
+            if "&ldquo;" in Alt[items]:
+                Alt[items]= Alt[items].replace("&ldquo;","")
+        for items in range(len(Alt)):
+            if "&rdquo;" in Alt[items]:
+                Alt[items]= Alt[items].replace("&rdquo;","")
+        for items in range(len(Alt)):
+            if "&#039" in Alt[items]:
+                Alt[items]= Alt[items].replace("&#039","")
         
-    elif svartf != Rätt and antalF<10:
-        antalF +=1
-        print("Tyvär är det fel, rätt svar är", Rätt+", vi går vidare till fråga nummer",antalF,"\n")
-    
-    elif svartf == Rätt and antalF == 10:
-        points +=1
-        print("Helt rätt, det var sista frågan. Här är ditt resultat\n")
-        break
-    
-    elif svartf != Rätt and antalF == 10:
-        print("Tyvär är det fel, det var sista frågan. Här är ditt resultat\n")
-        break
+        Alt.append(Rätt)
+        random.shuffle(Alt)     #blandar alternativen i arrayen så att rätt svar inte är på samma plats varje gång
+
+        if len(Alt) == 2:
+            print(Fråga + "\n",Alt[0]," - ",Alt[1])
+
+        elif len(Alt) == 3:
+            print(Fråga + "\n",Alt[0]," - ",Alt[1]," - ",Alt[2])        #ibland är det färre än fyra altenaativ. dessa if/elif satser gör att den bara skriver ut rätt antal alternativ
+        
+        elif len(Alt) == 4:
+            print(Fråga + "\n",Alt[0]," - ",Alt[1]," - ",Alt[2]," - ",Alt[3])
+
+        svartf = (input("> "))
+        svartf=svartf.title()
+
+        if svartf == Rätt and antalF<10:
+            antalF +=1
+            points +=1
+            print("Helt rätt, vidare till nästa fråga nummer",antalF,"\n")       
+            
+        elif svartf != Rätt and antalF<10:
+            antalF +=1
+            print("Tyvär är det fel, rätt svar är", Rätt+", vi går vidare till fråga nummer",antalF,"\n")
+        
+        elif svartf == Rätt and antalF == 10:
+            points +=1
+            print("Helt rätt, det var sista frågan. Här är ditt resultat:\nAv 10 möjliga fick du",points)
+            if points < 6:
+                print("Du kan bättre.\n")
+            else:
+                print("Bra jobbat!\n")
+            break
+        
+        elif svartf != Rätt and antalF == 10:
+            print("Tyvär är det fel, rätt svar är", Rätt+". Det var sista frågan, här är ditt resultat\nAv 10 möjliga fick du",points)
+            if points < 6:
+                print("Du kan bättre.\n")
+            else:
+                print("Bra jobbat!\n")
+            break
